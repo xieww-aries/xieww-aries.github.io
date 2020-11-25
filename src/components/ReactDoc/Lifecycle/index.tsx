4a2b755fda09cc444de1eee7ab84b5f1250d070f
xieww <xieww@trip.com>
2020-11-25 17:10:52 +0800

    feat: update

--- src/components/ReactDoc/Lifecycle/index.tsx
+++ src/components/ReactDoc/Lifecycle/index.tsx
@@ -6,6 +6,7 @@ export default function Lifecycle() {
         <div>
             <Title title={'React Lifecycle'} />
             <p>文档地址：<a href="https://segmentfault.com/a/1190000016617400">https://segmentfault.com/a/1190000016617400</a></p>
+            <p>流程指引图：https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/</p>
         </div>
     );
 }
