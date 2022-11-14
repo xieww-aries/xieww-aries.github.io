fa18659fe6d16b2436f04e35661a342c4224bd0d
xieweiwei21 <xieweiwei21@jd.com>
2022-11-14 20:20:01 +0800

    feat: update

---
+++ src/components/Home/Dashboard/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './style.scss';
+
+export default function Dashboard() {
+    const rotateValue = 80;
+    return (
+        <div styleName="wrapper">
+            <div styleName="outer">
+                <div styleName="mask" style={{ transform: `rotate(${rotateValue}deg)` }}>
+                    <div styleName="radius-box">
+                        <div styleName="radius"></div>
+                    </div>
+                </div>
+            </div>
+            <div styleName="inner">
+                <div styleName="arrow-box">
+                    <div styleName="arrow" style={{ transform: `rotate(${rotateValue}deg)` }}></div>
+                    <div styleName="anchor" style={{ transform: `rotate(${rotateValue}deg)` }}></div>
+                    <div styleName="inner-content">
+                        <p>限量1000件</p>
+                        <p>剩89件</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.scss';
 
 import Animation from '../../components/Home/3DAnimation';
+import Dashboard from '../../components/Home/Dashboard';
 
 import Title from '../../components/common/Title';
 
@@ -9,6 +10,7 @@ export default function Home() {
     return (
         <div styleName="home">
             <Title title={'目录'} />
+            <Dashboard />
             <Animation />
         </div>
     );
